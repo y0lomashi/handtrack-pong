6d53524e511e06d30d33e74f2a7167b5cbd32017
Curtis Li <curtisli7788@gmail.com>
2023-01-11 15:13:19 -0500

    Added multithreading to improve game performance

--- play.py
+++ play.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import cv2
 import mediapipe as mp
+import threading
 
 
 def update_fps():
@@ -182,198 +183,224 @@ cfps = int(cap.get(cv2.CAP_PROP_FPS))
 # Set resolution
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
 # Handtracking position variables
 left_y = 0
 right_y = 0
-# Set max num of hands detected
-# Set the percentage confidence needed to detect a hand (0-1)
-with mp_hands.Hands(model_complexity=0,
-                    max_num_hands=2,
-                    min_detection_confidence=0.5,
-                    min_tracking_confidence=0.5) as hands:
-    # main game loop
-    while True:
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+position = [0, 0, 0]
 
-        # To improve performance, optionally mark the image as not
-        # writeable to pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
-        # *Convert image to pixel measurement
-        image_height, image_width, _ = image.shape
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            if len(results.multi_handedness) == 2:
-                for i in range(len(results.multi_handedness)):
-                    if results.multi_handedness[i].classification[0].label == "Right":
-                        # * Hand point is 9
-                        # AKA .landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP]
-                        left_hand = results.multi_hand_landmarks[i].landmark[9]
-                    else:
-                        right_hand = results.multi_hand_landmarks[i].landmark[9]
-                # Getting y coordinates of the hand points
-                left_y = left_hand.y * image_height
-                right_y = right_hand.y * image_height
-            else:
-                left_hand = results.multi_hand_landmarks[0].landmark[9]
-                # Getting y coordinates of the hand points
-                left_y = left_hand.y * image_height
 
-            for hand_landmarks in results.multi_hand_landmarks:                    
-                # Drawing landmarks on the screen
-                mp_drawing.draw_landmarks(
-                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-        # * --- game logic ---
-        # keydowns and keyups raise and lower player paddles
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT:
-                cap.release()
-                pygame.quit()
-                exit()
-            # get players keys
-            p1_handle_event(event)
-            p2_handle_event(event)
+def fullTrack(postition):
+    """
+    This function is used to track the hand and draw the landmarks on the
+    screen.
+    """
+    # For webcam input:
+    cap = cv2.VideoCapture(0)
+    # Set size of webcam window
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    # Set max num of hands detected
+    # Set the percentage confidence needed to detect a hand (0-1)
+    with mp_hands.Hands(model_complexity=0,
+                        max_num_hands=2,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5) as hands:
+        while True:
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
 
-        # get other changes
-        p1_update()
-        p2_update()
+            # To improve performance, optionally mark the image as not
+            # writeable to pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+            # *Convert image to pixel measurement
+            image_height, image_width, _ = image.shape
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                if len(results.multi_handedness) == 2:
+                    for i in range(len(results.multi_handedness)):
+                        if results.multi_handedness[i].classification[
+                                0].label == "Right":
+                            # * Hand point is 9
+                            # AKA .landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP]
+                            left_hand = results.multi_hand_landmarks[
+                                i].landmark[9]
+                        else:
+                            right_hand = results.multi_hand_landmarks[
+                                i].landmark[9]
+                    # Getting y coordinates of the hand points
+                    postition[0] = left_hand.y * image_height
+                    postition[1] = right_hand.y * image_height
+                else:
+                    left_hand = results.multi_hand_landmarks[0].landmark[9]
+                    # Getting y coordinates of the hand points
+                    position[0] = left_hand.y * image_height
 
-        # move player pads to hand position
-        if left_y < p1_pad_y + 50:
-            p1_move_up = True
-            p1_move_down = False
-        elif left_y > p1_pad_y + 50:
-            p1_move_up = False
-            p1_move_down = True
-        else:  # stop
-            p1_move_up = False
-            p1_move_down = False
-        # move player pads to key press
-        if p1_move_up:
-            p1_pad_y -= PLAYER_PAD_SPEED
-            if p1_pad_y < 0:
-                p1_pad_y = 0
-        elif p1_move_down:
-            p1_pad_y += PLAYER_PAD_SPEED
-            if p1_pad_y > DISPLAY_HEIGHT - PLAYER_PAD_LENGTH:
-                p1_pad_y = DISPLAY_HEIGHT - PLAYER_PAD_LENGTH
-        # move player pads to hand position
-        if right_y < p2_pad_y + 50:
-            p2_move_up = True
-            p2_move_down = False
-        elif right_y > p2_pad_y + 50:
-            p2_move_up = False
-            p2_move_down = True
-        else:  # stop
-            p2_move_up = False
-            p2_move_down = False
-        # move player pads to key press
-        if p2_move_up:
-            p2_pad_y -= PLAYER_PAD_SPEED
-            if p2_pad_y < 0:
-                p2_pad_y = 0
-        elif p2_move_down:
-            p2_pad_y += PLAYER_PAD_SPEED
-            if p2_pad_y > DISPLAY_HEIGHT - PLAYER_PAD_LENGTH:
-                p2_pad_y = DISPLAY_HEIGHT - PLAYER_PAD_LENGTH
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Drawing landmarks on the screen
+                    mp_drawing.draw_landmarks(
+                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+            # Flip the image horizontally for a selfie-view display.
+            position[2] = image
 
-        # move ball
-        ball_x += ball_speed_x
-        ball_y += ball_speed_y
 
-        # check ball position
-        # if out screen vertically, flip ball_speed_y
-        if ball_y < 0 or ball_y > DISPLAY_HEIGHT - BALL_RADIUS:
-            ball_speed_y = -ball_speed_y
+# Start the thread
+thread = threading.Thread(target=fullTrack, args=(position, ))
+thread.start()
 
-        # if out screen horizontally, check whether player pad is there or not
-        # if not, release the ball at the center towards scoring player
-        if ball_x < 0:
-            if p1_pad_y < ball_y < p1_pad_y + PLAYER_PAD_LENGTH:
-                ball_speed_x = -ball_speed_x
-            else:
-                p2_score += 1
-                ball_x = 400
-                ball_y = 300
-                ball_speed_x = 7.5
-                ball_speed_y = 7.5
-        elif ball_x > DISPLAY_WIDTH:
-            if p2_pad_y < ball_y < p2_pad_y + PLAYER_PAD_LENGTH:
-                ball_speed_x = -ball_speed_x
-            else:
-                p1_score += 1
-                ball_x = 400
-                ball_y = 300
-                ball_speed_x = -7.5
-                ball_speed_y = -7.5
+# * --- game logic ---
+while True:
+    left_y = position[0]
+    right_y = position[1]
+    # keydowns and keyups raise and lower player paddles
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            cap.release()
+            pygame.quit()
+            exit()
+        # get players keys
+        p1_handle_event(event)
+        p2_handle_event(event)
 
-        # clear the screen
-        screen.fill(pygame.Color(0, 0, 0, 255))
+    # get other changes
+    p1_update()
+    p2_update()
 
-        # draw ball
-        pygame.draw.circle(screen, pygame.Color(255, 255, 255, 255),
-                           (ball_x, ball_y), BALL_RADIUS)
+    # move player pads to hand position
+    if left_y < p1_pad_y - 20:
+        p1_move_up = True
+        p1_move_down = False
+    elif left_y > p1_pad_y + 20:
+        p1_move_up = False
+        p1_move_down = True
+    else:  # stop
+        p1_move_up = False
+        p1_move_down = False
+    # move player pads to key press
+    if p1_move_up:
+        p1_pad_y -= PLAYER_PAD_SPEED
+        if p1_pad_y < 0:
+            p1_pad_y = 0
+    elif p1_move_down:
+        p1_pad_y += PLAYER_PAD_SPEED
+        if p1_pad_y > DISPLAY_HEIGHT - PLAYER_PAD_LENGTH:
+            p1_pad_y = DISPLAY_HEIGHT - PLAYER_PAD_LENGTH
+    # move player pads to hand position
+    if right_y < p2_pad_y - 20:
+        p2_move_up = True
+        p2_move_down = False
+    elif right_y > p2_pad_y + 20:
+        p2_move_up = False
+        p2_move_down = True
+    else:  # stop
+        p2_move_up = False
+        p2_move_down = False
+    # move player pads to key press
+    if p2_move_up:
+        p2_pad_y -= PLAYER_PAD_SPEED
+        if p2_pad_y < 0:
+            p2_pad_y = 0
+    elif p2_move_down:
+        p2_pad_y += PLAYER_PAD_SPEED
+        if p2_pad_y > DISPLAY_HEIGHT - PLAYER_PAD_LENGTH:
+            p2_pad_y = DISPLAY_HEIGHT - PLAYER_PAD_LENGTH
 
-        # draw P1 pad
-        pygame.draw.rect(
-            screen,
-            pygame.Color(255, 255, 255, 255),
-            (0, p1_pad_y, PLAYER_PAD_WIDTH, PLAYER_PAD_LENGTH),
-        )
+    # move ball
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
 
-        # draw P2 pad
-        pygame.draw.rect(
-            screen,
-            pygame.Color(255, 255, 255, 255),
-            (
-                DISPLAY_WIDTH - PLAYER_PAD_WIDTH,
-                p2_pad_y,
-                PLAYER_PAD_WIDTH,
-                PLAYER_PAD_LENGTH,
-            ),
-        )
+    # check ball position
+    # if out screen vertically, flip ball_speed_y
+    if ball_y < 0 or ball_y > DISPLAY_HEIGHT - BALL_RADIUS:
+        ball_speed_y = -ball_speed_y
 
-        # draw center line
-        pygame.draw.rect(
-            screen,
-            pygame.Color(255, 255, 255, 255),
-            (DISPLAY_WIDTH / 2, 0, 1, DISPLAY_HEIGHT),
-        )
+    # if out screen horizontally, check whether player pad is there or not
+    # if not, release the ball at the center towards scoring player
+    if ball_x < 0:
+        if p1_pad_y < ball_y < p1_pad_y + PLAYER_PAD_LENGTH:
+            ball_speed_x = -ball_speed_x
+        else:
+            p2_score += 1
+            ball_x = 400
+            ball_y = 300
+            ball_speed_x = 5
+            ball_speed_y = 5
+    elif ball_x > DISPLAY_WIDTH:
+        if p2_pad_y < ball_y < p2_pad_y + PLAYER_PAD_LENGTH:
+            ball_speed_x = -ball_speed_x
+        else:
+            p1_score += 1
+            ball_x = 400
+            ball_y = 300
+            ball_speed_x = -5
+            ball_speed_y = -5
 
-        # draw player scores
-        # create font
-        score_font = pygame.font.Font(None, 30)
+    # clear the screen
+    screen.fill(pygame.Color(0, 0, 0, 255))
 
-        # draw p1 score
-        p1_score_text = str(p1_score)
-        p1_score_render = score_font.render(p1_score_text, 1,
-                                            pygame.Color(255, 255, 255, 255))
-        screen.blit(p1_score_render, (DISPLAY_WIDTH / 2 - 50, 50))
+    # draw ball
+    pygame.draw.circle(screen, pygame.Color(255, 255, 255, 255),
+                       (ball_x, ball_y), BALL_RADIUS)
 
-        # draw p2 score
-        p2_score_text = str(p2_score)
-        p2_score_render = score_font.render(p2_score_text, 1,
-                                            pygame.Color(255, 255, 255, 255))
-        screen.blit(p2_score_render, (DISPLAY_WIDTH / 2 + 50, 50))
+    # draw P1 pad
+    pygame.draw.rect(
+        screen,
+        pygame.Color(255, 255, 255, 255),
+        (0, p1_pad_y, PLAYER_PAD_WIDTH, PLAYER_PAD_LENGTH),
+    )
 
-        # update fps counter
-        screen.blit(update_fps(), (10, 0))
+    # draw P2 pad
+    pygame.draw.rect(
+        screen,
+        pygame.Color(255, 255, 255, 255),
+        (
+            DISPLAY_WIDTH - PLAYER_PAD_WIDTH,
+            p2_pad_y,
+            PLAYER_PAD_WIDTH,
+            PLAYER_PAD_LENGTH,
+        ),
+    )
 
-        # pygame.display.flip() is called in order to update graphics properly
-        pygame.display.flip()
+    # draw center line
+    pygame.draw.rect(
+        screen,
+        pygame.Color(255, 255, 255, 255),
+        (DISPLAY_WIDTH / 2, 0, 1, DISPLAY_HEIGHT),
+    )
 
-        # tick the clock so we have 30 fps game
-        clock.tick(30)
+    # draw player scores
+    # create font
+    score_font = pygame.font.Font(None, 30)
+
+    # draw p1 score
+    p1_score_text = str(p1_score)
+    p1_score_render = score_font.render(p1_score_text, 1,
+                                        pygame.Color(255, 255, 255, 255))
+    screen.blit(p1_score_render, (DISPLAY_WIDTH / 2 - 50, 50))
+
+    # draw p2 score
+    p2_score_text = str(p2_score)
+    p2_score_render = score_font.render(p2_score_text, 1,
+                                        pygame.Color(255, 255, 255, 255))
+    screen.blit(p2_score_render, (DISPLAY_WIDTH / 2 + 50, 50))
+
+    # update fps counter
+    screen.blit(update_fps(), (10, 0))
+
+    # pygame.display.flip() is called in order to update graphics properly
+    pygame.display.flip()
+
+    # tick the clock for certain amount of fps
+    clock.tick(60)
+    cv2.imshow('MediaPipe Hands', cv2.flip(position[2], 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
