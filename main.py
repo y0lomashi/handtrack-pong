244e43cc40d11398674d19727707a8991663feb7
Curtis Li <curtisli7788@gmail.com>
2023-01-24 12:43:53 -0500

    Added header file to main file.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Description: AI webcam handtracking pong game. Control the paddle with your
+#             hand. The game will start with a main menu, and you can chose 
+#             between 3 different game modes. The game will end when the window
+#             is closed.
+# Name: Curtis Li
+# Date: 2023-01-01
+# Version: 1.0
+
 import menu
 
 
